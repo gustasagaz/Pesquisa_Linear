362b2324386e1033f0b418d344f5a48ab87ed32a
Gustavo Willian Sagaz <gustawilliansagaz@gmail.com>
2026-03-24 20:41:25 -0300

    Agora ele para quando o elemento procurado

--- src/main/java/Exercicio01.java
+++ src/main/java/Exercicio01.java
@@ -1,22 +1,35 @@
 import javax.swing.*;
+
 public class Exercicio01 {
     public static void main(String args[]) {
         int i, flag;
-        int numElementos = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de pessoas a ser cadastrado "));
+
+        int numElementos = Integer.parseInt(
+                JOptionPane.showInputDialog("Digite o número de pessoas a ser cadastrado ")
+        );
+
         String vetorPesquisado[] = new String[numElementos];
-        for(i=0; i<numElementos;i++){
+
+        for(i = 0; i < numElementos; i++){
             vetorPesquisado[i] = JOptionPane.showInputDialog("Digite o nome para cadastro");
         }
+
         String elementoProcurado = JOptionPane.showInputDialog("Digite o nome a ser procurado");
+
         flag = 0;
-        for(i=0; i<numElementos;i++){
-            if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)){
-                JOptionPane.showMessageDialog(null,"o valor procurado foi encontrado na posição "+i);
+        i = 0;
+
+        while (i < numElementos && flag == 0) {
+            if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)) {
+                JOptionPane.showMessageDialog(null,
+                        "O valor procurado foi encontrado na posição " + i);
                 flag = 1;
             }
+            i++;
         }
+
         if (flag == 0){
-            JOptionPane.showMessageDialog(null, "o nome não foi encontrado.");
+            JOptionPane.showMessageDialog(null, "O nome não foi encontrado.");
         }
     }
 }
